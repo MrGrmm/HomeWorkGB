d402c321ec24afceaa2921c4167541cff172de72
Gramma <gramma1997ion@gmail.com>
2022-10-18 09:53:28 +0300

    Solved the 1th task of sem5 C#

--- Program.cs
+++ Program.cs
@@ -320,3 +320,42 @@ ShowArray(myArray);
 
 */
 
+//Семинар 5
+
+//Задача№1 Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] newArray = new int[size];
+
+    for(int i = 0; i < size; i++)
+        newArray[i] = new Random().Next(minValue, maxValue + 1);
+
+    return newArray;
+}
+int FindEvenNumbers(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++) 
+        if (array[i] % 2 == 0)
+            count++; 
+    return count;
+}
+
+Console.WriteLine("Enter the size of the array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateRandomArray(size, 100, 1000);
+ShowArray(myArray);
+int evens = FindEvenNumbers(myArray);
+Console.WriteLine($"There are {evens} even numbers in this array");
+
+//Задача№2 Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+//Задача№3 Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
